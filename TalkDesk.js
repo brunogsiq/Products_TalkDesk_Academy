234438e864030375d8d15a89681e0e2139c6a791
brunogsiq <brunogsiq@gmail.com>
2023-12-13 20:19:05 -0300

    New structure project

---
+++ TalkDesk.js
@@ -0,0 +1,41 @@
+/*
+Explore o site oficial do Talkdesk para obter uma visão geral da plataforma, seus recursos e benefícios.
+Documentação e Recursos Iniciais:
+
+Acesse a documentação oficial do Talkdesk para entender os conceitos básicos, configuração inicial e recursos principais.
+Trailhead para Talkdesk:
+
+Consulte se há módulos ou tutoriais no Trailhead específicos para o Talkdesk, se disponíveis.
+Configuração Básica:
+
+Use os recursos fornecidos para configurar uma conta básica no Talkdesk. Experimente as configurações principais, como roteamento de chamadas, configuração do IVR, etc.
+Treinamento Online:
+
+Procure webinars, vídeos de treinamento ou tutoriais online oferecidos pelo Talkdesk ou por especialistas na plataforma.
+Certificações e Cursos:
+
+Verifique se o Talkdesk oferece certificações ou cursos de treinamento. Esses programas podem ajudar a aprofundar seu conhecimento na plataforma.
+Integrações:
+
+Explore como o Talkdesk se integra a outras ferramentas ou CRMs, como Salesforce. Isso é crucial para entender como o Talkdesk pode se encaixar no seu ambiente de trabalho.
+Testes Práticos:
+
+Crie testes práticos para si mesmo. Experimente diferentes cenários de atendimento ao cliente, ajuste configurações e veja como a plataforma responde.
+Suporte e Comunidade:
+
+Participe de fóruns e comunidades online relacionados ao Talkdesk. Isso pode ser útil para obter suporte, compartilhar experiências e aprender com outros usuários.
+Projetos Práticos:
+
+Aplique seus conhecimentos em projetos práticos. Configure o Talkdesk para atender às necessidades específicas da sua organização.
+Atualizações e Novas Funcionalidades:
+Fique atento a atualizações e novas funcionalidades do Talkdesk. Isso pode ser feito através de blogs, newsletters ou fóruns da comunidade.
+Networking:
+Participe de eventos do setor, webinars e conferências relacionadas a centros de contato e soluções de atendimento ao cliente.
+Lembre-se de que a prática é fundamental ao aprender qualquer nova ferramenta. Trabalhe em projetos práticos e experimente diferentes recursos para solidificar seu conhecimento no Talkdesk. Boa sorte!
+*/
+
+/*
+Visão Geral: Talkdesk é uma solução de centro de contato baseada em nuvem que fornece ferramentas para suporte e engajamento do cliente. Oferece recursos como roteamento de chamadas, resposta interativa de voz (IVR), análises e integrações com vários CRMs, incluindo o Salesforce.
+Recursos: O Talkdesk visa otimizar as interações do cliente por meio de vários canais, incluindo voz, chat e e-mail. Enfatiza a flexibilidade e a escalabilidade para empresas de diferentes tamanhos.
+Perspectiva Interna: Discussões internas sobre o Talkdesk podem envolver a facilidade de configuração, integração com sistemas existentes, experiência do usuário para agentes e clientes, e a capacidade da plataforma de se adaptar a diferentes setores e casos de uso.
+*/
